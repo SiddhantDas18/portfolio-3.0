4c5ce3049563a964bb439611cc009dbdae08ad59
siddhantdas18 <siddhantdas18@gmail.com>
2025-04-18 15:09:22 +0530

    Favicon

--- Sections/Projects.tsx
+++ Sections/Projects.tsx
@@ -5,8 +5,19 @@ export default function Projects() {
                 <p className="bg-grad">Projects</p>
             </div>
 
-            <div>
-                
+            <div className="pt-5">
+                <div>
+                    
+                    <a href="https://github.com/SiddhantDas18/Colaborative-compiler" target="blank"><h1 className="text-xl">Collaborative Code Editor</h1></a>
+                    <p className="pt-2 pl-2 opacity-70">Collaborative code editor made using React, where we can create roomId and join them. And can collaboratively edit our Code</p>
+                </div>
+
+                <div className="pt-2">
+                    <a href="https://github.com/SiddhantDas18/BasicUI-Rewritting" target="blank" className="text-xl"><h1>Typing UI</h1></a>
+                    <p className="pt-2 pl-2 opacity-70">Rich Ui element just like the one below.</p>
+                </div>
+
+
             </div>
         </div>
     </section>
