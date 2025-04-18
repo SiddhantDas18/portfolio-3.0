edec12d82aec8ab20b8214881272a43a1de2520b
siddhantdas18 <siddhantdas18@gmail.com>
2025-04-18 15:20:10 +0530

    Fix the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from 'next/head';
 
 import "./globals.css";
 import Navbar from "@/Components/Header";
@@ -26,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
       >
