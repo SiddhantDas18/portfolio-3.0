a0d9a79b4b2574061a596f426e5968e2f07c2f7c
siddhantdas18 <siddhantdas18@gmail.com>
2025-05-20 20:31:40 +0530

    fixed top tracs area and put tha ton the middle for the desktop

--- app/page.tsx
+++ app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
       
 
       <div className="w-full max-w-[1400px] px-3 sm:px-4 md:px-6 lg:px-8 mt-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="flex flex-col gap-8">
+        <div className="flex justify-center">
+          <div className="flex flex-col gap-8 w-full max-w-xl">
             <SpotifyTopTracks />
           </div>
         </div>
